47aea368155449e21f429c4c29a553e714fda915
Yves King Chandra <ychandra02@student.ciputra.ac.id>
2021-12-03 11:20:25 +0800

    Cek Bilangan Prima

--- GitHub Test Prime Number/Program.cs
+++ GitHub Test Prime Number/Program.cs
@@ -6,7 +6,25 @@ namespace GitHub_Test_Prime_Number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int input, a = 0;
+            Console.Write("Masukkan Input: ");
+            input = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            for (int i = 1; i <= input; i++)
+            {
+                if (input % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("{0} adalah bilangan prima", input);
+            }
+            else
+            {
+                Console.WriteLine("{0} bukan bilangan prima", input);
+            }
         }
     }
 }
